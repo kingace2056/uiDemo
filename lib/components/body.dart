3a42712d365ecce8626ccb379c4aeb9d4f27757b
kingace2056 <sarthak123parajuli@gmail.com>
2021-11-30 21:49:03 +0545

    search bar has been places on proper place.

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ui_practise/constraints.dart';
 
 class Body extends StatelessWidget {
@@ -6,23 +7,61 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // this will provide the dimension of the screen
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        Container(
-          // this will fill 20% of our total screen
-          height: size.height * 0.2,
-          decoration: const BoxDecoration(
-            color: kPrimaryColor,
-            //border radius only will provide us only the specified border to be curved in given radius
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(30),
-              bottomRight: Radius.circular(30),
-            ),
-          ),
-        )
-      ],
-    );
+    return Column(children: <Widget>[
+      Container(
+        height: size.height * 0.2,
+        child: Stack(
+          children: <Widget>[
+            Container(
+                height: size.height * 0.2 - 27,
+                decoration: BoxDecoration(
+                    color: kPrimaryColor,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(36),
+                      bottomRight: Radius.circular(36),
+                    ))),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  height: 54,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 50,
+                          color: kPrimaryColor,
+                        )
+                      ]),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          onChanged: (value) {},
+                          decoration: InputDecoration(
+                            hintText: "Search",
+                            hintStyle: TextStyle(
+                              color: kPrimaryColor.withOpacity(0.5),
+                            ),
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      SvgPicture.asset("assets/icons/search.svg")
+                    ],
+                  )),
+            )
+          ],
+        ),
+      )
+    ]);
   }
 }
