2d7e9dfcb47db838cd1b0ac6421c8d3ab3fb5223
kingace2056 <sarthak123parajuli@gmail.com>
2021-11-30 21:49:34 +0545

    opacity changes

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -48,7 +48,7 @@ class Body extends StatelessWidget {
                           decoration: InputDecoration(
                             hintText: "Search",
                             hintStyle: TextStyle(
-                              color: kPrimaryColor.withOpacity(0.5),
+                              color: kPrimaryColor.withOpacity(0.55),
                             ),
                             enabledBorder: InputBorder.none,
                             focusedBorder: InputBorder.none,
