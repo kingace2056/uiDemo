25adb150805a259348d645d6deaa70d1a178e46d
kingace2056 <sarthak123parajuli@gmail.com>
2021-12-28 19:08:24 +0545

    added const modifier whereever needed 😊

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -9,13 +9,13 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Column(children: <Widget>[
-      Container(
+      SizedBox(
         height: size.height * 0.2,
         child: Stack(
           children: <Widget>[
             Container(
                 height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                     color: kPrimaryColor,
                     borderRadius: BorderRadius.only(
                       bottomLeft: Radius.circular(36),
@@ -27,13 +27,13 @@ class Body extends StatelessWidget {
               right: 0,
               child: Container(
                   alignment: Alignment.center,
-                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
                   height: 54,
                   decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
+                      boxShadow: const [
                         BoxShadow(
                           offset: Offset(0, 10),
                           blurRadius: 50,
